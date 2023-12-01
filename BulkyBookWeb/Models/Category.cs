ec570e74c1d2a98ebf91bce09ca9584bc5e6584d
Sahil Tushamar <sahil8713@gmail.com>
2023-12-01 15:26:36 +0530

    Modify files in accordance to editorconfig file

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,20 +1,19 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyBookWeb.Models
+namespace BulkyBookWeb.Models;
+
+public class Category
 {
-    public class Category
-    {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        [MaxLength(50)]
-        [DisplayName("Name")]
-        public string Name { get; set; } = string.Empty;
-        [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage="Display Order must be between 1 and 100.")]
-        public int DisplayOrder { get; set; }
-        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
-        
-    }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [DisplayName("Name")]
+    public string Name { get; set; } = string.Empty;
+    [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage="Display Order must be between 1 and 100.")]
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    
 }
